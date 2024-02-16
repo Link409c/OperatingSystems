304f7e4c5fdd3cb98797903c721dc5da90da345d
CSimpson <cdsimpsonnavy2010@yahoo.com>
2024-02-15 23:07:00 -0500

    pseudocode for P2

---
+++ P2/MemoryManager.py
@@ -0,0 +1,24 @@
+# Project 2
+# The Memory Manager
+# Christian Simpson
+##
+# This program emulates the different memory management schemes in Operating Systems which allocate
+# system memory to user jobs, programs, and processes.
+#
+# Pseudocode for the program:
+#
+# The Program should
+# Process a list of jobs and their memory costs
+# create a list of jobs in order of their submission
+# allocate memory in different ways
+#   Best-Fit
+#   First-Fit
+# the program should keep several lists:
+#   submitted jobs
+#   memory partitions
+#   which job is at what partition
+#   working jobs
+#   completed jobs
+# the program should adjust memory segments using compaction.
+# the program should print the lists to a file?
+# the program should print some type of results for the user?
