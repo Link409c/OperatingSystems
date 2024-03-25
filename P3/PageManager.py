1dc365feb936a4c20e0c1364573792ec307235ed
CSimpson <cdsimpsonnavy2010@yahoo.com>
2024-03-25 13:26:43 -0400

    project 3 work 3/25

--- P3/PageManager.py
+++ P3/PageManager.py
@@ -5,20 +5,6 @@
 # The program will use given data from the user to determine the best policy to handle
 # the needs of their system.
 
-# sample run of program:
-#
-# get the list of jobs as input
-# divide each job into pages
-# assign each page a frame until no more frames available
-# for each job, follow its list of page calls to complete it
-# for each step,
-# check frames to see if that page is loaded
-# if it is, move to next step
-# if not, use the appropriate removal policy to load it into a frame,
-# then increment an interrupt counter.
-# record the amount of interrupts when using FIFO, then LRU.
-# print the results.
-
 import sys
 import random
 
@@ -70,11 +56,81 @@ class Job:
         # this is the steps of how to complete the job.
         return steps
 
+class PageTableEntry:
+    """
+    An object representing a single entry in a Page Management Table.
+    Has flags for wether the page is stored in a page frame, if the page
+    has been referenced since being loaded into memory, if the page has been
+    modified since being loaded into memory, and the frame number the page is
+    stored at.
+    """
+    __slots__ = ['inMemory', 'referenced', 'modified', 'frameNumber']
+
+    def __init__(self):
+        self.inMemory = False
+        self.referenced = 0
+        self.modified = 0
+        self.frameNumber = None
+    def __init__(self, inMemoryflag, referenceflag, modifiedflag, aframeNumber):
+        self.inMemory = inMemoryflag
+        self.referenced = referenceflag
+        self.modified = modifiedflag
+        self.frameNumber = aframeNumber
+def makeNewFrames(numframes, jobs = [Job]):
+    """
+    creates a new list of frames according to the passed parameter,
+    and populates the list with as many pages as possible.
+    """
+    # make list of frames
+    frames = [None for _ in range(numframes)]
+    # for each job,
+    for Job in jobs:
+        # if there are empty frames,
+        if frames.__contains__(None):
+            # for each page in the job,
+            for page in Job.pages:
+                # assign pages to empty frames
+                frames[frames.index(None)] = page
+        else:
+            break
+    # return frames once filled with pages
+    return frames
+
+
 def assignframeFIFO(page):
     """assigns a page of a job to a frame in memory, removing pages using FIFO policy."""
 
 def assignframeLRU(page):
     """assigns a page of a job to a frame in memory, removing pages using LRU policy."""
+
+def runprogram(inputfile):
+    print("Welcome to the Page Management Program.")
+    # create jobs from file input
+    jobs = []
+    with open(inputfile, 'r') as file:
+        # get each number in sequence
+        file.read()
+        # these are the jobs' memory requirements
+        # for each number, make a new job object
+
+    # get number of page frames (variable user input)
+    numframes = input("\nEnter the number of page frames you want to test with.")
+    frames = makeNewFrames()
+    # list representing page management table for each job
+    pmts = [[None] for _ in range(len(jobs))]
+    # divide each job into pages
+    # assign each page a frame until no more frames available
+    # for each job, follow its list of page calls to complete it
+    # for each step,
+    # check frames to see if that page is loaded
+    # if it is, move to next step
+    # if not, use the appropriate removal policy to load it into a frame,
+    # then increment an interrupt counter.
+    # record the amount of interrupts when using FIFO, then LRU.
+    # print the results.
+
+if __name__ == "__main__":
+    runprogram(sys.argv[1])
 
 
 
