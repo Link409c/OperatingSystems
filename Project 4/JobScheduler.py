a6293305454389142a90e70a9de1996bae39e086
CSimpson <cdsimpsonnavy2010@yahoo.com>
2024-04-12 16:46:11 -0400

    initial commit for job scheduler program.

--- Project 4/JobScheduler.py
+++ Project 4/JobScheduler.py
@@ -1,2 +1,8 @@
 # From Flynn & McHoes 8th ed.
 # Pg 139 Exercise 19
+# Write a program that will simulate FCFS, SJN, SRT, and round robin
+# scheduling algorithms. For each algorithm, the program should compute waiting
+# time and turnaround time for every job, as well as the average waiting time
+# and average turnaround time. The average values should be consolidated in a
+# table for easy comparison. You may use the following data to test your program.
+# The time quantum for the round robin is 4 ms. (Assume that the context switching time is 0).
